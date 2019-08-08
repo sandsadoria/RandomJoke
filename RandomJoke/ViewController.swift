b9d3d5ce4796c2d316ae05fcf1f0b37d224bddfd
Thawatchai Phuchana <sandsadoria@gmail.com>
2019-08-08 14:28:38 +0700

    Initial Commit

---
+++ RandomJoke/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomJoke
+//
+//  Created by Thawatchai Phuchana on 8/8/2562 BE.
+//  Copyright © 2562 Thawatchai Phuchana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
